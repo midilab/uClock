2ac7a5eebb4951cd791063c171b55cff6790cd64
mrms <16727232+m-r-m-s@users.noreply.github.com>
2022-08-11 14:37:38 -0400

    Update TeensyUsbSlaveMidiClock.ino
    remove volatile reference pointer to avoid compilation error

--- examples/TeensyUsbSlaveMidiClock/TeensyUsbSlaveMidiClock.ino
+++ examples/TeensyUsbSlaveMidiClock/TeensyUsbSlaveMidiClock.ino
@@ -21,7 +21,7 @@
 #include <uClock.h>
 
 uint8_t bpm_blink_timer = 1;
-void handle_bpm_led(uint32_t * tick)
+void handle_bpm_led(uint32_t tick)
 {
   // BPM led indicator
   if ( !(tick % (96)) || (tick == 1) ) {  // first compass step will flash longer
