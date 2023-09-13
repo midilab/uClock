3ccc0632c337e2c945ecd183dd102320eb8ca0fc
Jackson-Devices <123213412+Jackson-Devices@users.noreply.github.com>
2023-09-13 17:34:58 +0100

    Update LeonardoUsbSlaveMidiClockMonitor.ino
    Minor spelling corrections

--- examples/LeonardoUsbSlaveMidiClockMonitor/LeonardoUsbSlaveMidiClockMonitor.ino
+++ examples/LeonardoUsbSlaveMidiClockMonitor/LeonardoUsbSlaveMidiClockMonitor.ino
@@ -130,17 +130,17 @@ void loop() {
   if (clock_state != uClock.state) { 
     clock_state = uClock.state;
     if (clock_state >= 1) {
-      u8x8->drawUTF8(0, 7, "playing"); 
+      u8x8->drawUTF8(0, 7, "Playing"); 
     } else { 
-      u8x8->drawUTF8(0, 7, "stoped "); 
+      u8x8->drawUTF8(0, 7, "Stopped"); 
     }
   }
   if (clock_mode != uClock.getMode()) {
     clock_mode = uClock.getMode();
     if (clock_mode == uClock.EXTERNAL_CLOCK) {
-      u8x8->drawUTF8(10, 0, "slave "); 
+      u8x8->drawUTF8(10, 0, "Slave "); 
     } else { 
-      u8x8->drawUTF8(10, 0, "master"); 
+      u8x8->drawUTF8(10, 0, "Master"); 
     }
   }
 }
